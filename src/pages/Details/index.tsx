70cb96090a8eeca674a1bac6e3f23779a3d42621
Anderson Campos <anderleitec@gmail.com>
2020-10-05 13:55:55 -0300

    Get details route params

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import { CgPokemon } from 'react-icons/cg';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { CgPokemon, CgArrowLeft } from 'react-icons/cg';
 
-import { Container, Title } from './styles';
+import { Container, Header, Title, Content } from './styles';
+import api from '../../services/api';
+import formatString from '../../utils/formatString';
+
+interface IDetailsParams {
+  pokemon: string;
+}
+
+interface IPokemon {
+  id: number;
+  name: string;
+  imageURL: string;
+}
 
 const Details: React.FC = () => {
+  const [pokemons, setPokemons] = useState<IPokemon>({} as IPokemon);
+
+  const { params } = useRouteMatch<IDetailsParams>();
+
+  useEffect(() => {
+    async function loadDetails(): Promise<void> {
+      const response = await api.get(`/pokemon/${params.pokemon}`);
+
+      setPokemons(response.data);
+    }
+    loadDetails();
+  }, [params.pokemon]);
+
   return (
     <Container>
-      <Title>
-        P<CgPokemon size={30} /> kefinder
-      </Title>
+      <Header>
+        <Title>
+          P<CgPokemon size={30} /> kefinder
+        </Title>
+
+        <Link to="/">
+          <CgArrowLeft size={24} />
+          Voltar
+        </Link>
+      </Header>
+
+      <Content>
+        <div>
+          <h1>{pokemons.name}</h1>
+          <h1># {pokemons.id}</h1>
+        </div>
+      </Content>
     </Container>
   );
 };
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -6,9 +6,34 @@ export const Container = styled.div`
   padding: 0 32px;
 `;
 
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    text-decoration: none;
+    color: var(--text-color);
+    transition: color 0.2s;
+
+    &:hover {
+      color: #999;
+    }
+
+    svg {
+      margin-right: 8px;
+    }
+  }
+`;
+
 export const Title = styled.h1`
   font-size: 40px;
   color: var(--text-color);
   display: flex;
   align-items: center;
 `;
+
+export const Content = styled.div``;
